602ebe42d27de0c552a4a0fbe411d9deba80f237
Olgerti Xhanej <33934387+gerti98@users.noreply.github.com>
2020-11-09 23:54:27 +0100

    Implementati i metodi per il form-checking

--- src/it/unipi/dii/lsmsd/pokeMongo/userInterface/SignUp.java
+++ src/it/unipi/dii/lsmsd/pokeMongo/userInterface/SignUp.java
@@ -2,6 +2,7 @@ package it.unipi.dii.lsmsd.pokeMongo.userInterface;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
+import java.util.regex.*;
 
 public class SignUp extends PokeSceneWithTitle {
     // LEFT SIDE
@@ -10,11 +11,24 @@ public class SignUp extends PokeSceneWithTitle {
     private TextField passwordTF;
     private DatePicker birthdayDP;
 
+    private Label invalidSurnameLabel;
+    private Label invalidPasswordLabel;
+
     // RIGHT SIDE
     private TextField nameTF;
     private TextField emailTF;
     private TextField confirmPasswordTF;
     private TextField countryTF;
+
+    private Label invalidNameLabel;
+    private Label invalidEmailLabel;
+    private Label invalidConfirmPasswordLabel;
+
+
+    private final String styleInvalidFormEntry = "-fx-font-family: 'Arial'; -fx-font-size: 12px; " +
+            "-fx-background-color: rgb(255, 33, 26); -fx-text-fill: white; -fx-max-width: 230; -fx-padding: 3;" +
+            " -fx-border-radius: 15px; -fx-background-radius: 15px; -fx-border-width: 0.06em;" +
+            " -fx-border-color: black;";
 
     public SignUp() {
         displaySurnameFields();
@@ -38,15 +52,18 @@ public class SignUp extends PokeSceneWithTitle {
         surnameLabel.relocate(350, 170);
         surnameLabel.setStyle("-fx-font-family: 'Arial'; -fx-font-size: 20px; -fx-font-weight: bold;");
 
+        invalidSurnameLabel = new Label("Surname must only contain letters,\nspaces, dots and apostrophes");
+        invalidSurnameLabel.relocate(510, 190);
+        invalidSurnameLabel.setVisible(false);
+        invalidSurnameLabel.setStyle(styleInvalidFormEntry);
+
         surnameTF = new TextField();
         surnameTF.relocate(350, 200);
-        surnameTF.setOnKeyTyped(e->handleMouseClicked());
-        sceneNodes.getChildren().add(surnameLabel);
-        sceneNodes.getChildren().add(surnameTF);
-    }
+        surnameTF.setOnKeyReleased(e->handleSurname());
 
-    private void handleMouseClicked(){
-        System.out.println("Hello World");
+        sceneNodes.getChildren().add(surnameLabel);
+        sceneNodes.getChildren().add(invalidSurnameLabel);
+        sceneNodes.getChildren().add(surnameTF);
     }
 
     private void displayNicknameFields() {
@@ -66,10 +83,18 @@ public class SignUp extends PokeSceneWithTitle {
         passwordLabel.relocate(350, 370);
         passwordLabel.setStyle("-fx-font-family: 'Arial'; -fx-font-size: 20px; -fx-font-weight: bold;");
 
+
+        invalidPasswordLabel = new Label("Password must contain at least:\n8 characters, a letter and\na number");
+        invalidPasswordLabel.relocate(510, 380);
+        invalidPasswordLabel.setVisible(false);
+        invalidPasswordLabel.setStyle(styleInvalidFormEntry);
+
         passwordTF = new PasswordField();
         passwordTF.relocate(350, 400);
+        passwordTF.setOnKeyReleased(e->handlePassword());
 
         sceneNodes.getChildren().add(passwordLabel);
+        sceneNodes.getChildren().add(invalidPasswordLabel);
         sceneNodes.getChildren().add(passwordTF);
     }
 
@@ -91,22 +116,37 @@ public class SignUp extends PokeSceneWithTitle {
         nameLabel.relocate(750, 170);
         nameLabel.setStyle("-fx-font-family: 'Arial'; -fx-font-size: 20px; -fx-font-weight: bold;");
 
+
+        invalidNameLabel = new Label("Name must only contain letters,\nspaces, dots and apostrophes");
+        invalidNameLabel.relocate(910, 190);
+        invalidNameLabel.setVisible(false);
+        invalidNameLabel.setStyle(styleInvalidFormEntry);
+
         nameTF = new TextField();
         nameTF.relocate(750, 200);
+        nameTF.setOnKeyReleased(e->handleName()); //TODO: maybe setOnKeyReleased event trigger it's not the best choice
 
         sceneNodes.getChildren().add(nameLabel);
+        sceneNodes.getChildren().add(invalidNameLabel);
         sceneNodes.getChildren().add(nameTF);
     }
 
     private void displayEmailFields() {
-        Label emailLabel = new Label("Name");
+        Label emailLabel = new Label("Email");
         emailLabel.relocate(750, 270);
         emailLabel.setStyle("-fx-font-family: 'Arial'; -fx-font-size: 20px; -fx-font-weight: bold;");
 
+        invalidEmailLabel = new Label("Email must follow the format:\nexample@domain.tld");
+        invalidEmailLabel.relocate(910, 290);
+        invalidEmailLabel.setVisible(false);
+        invalidEmailLabel.setStyle(styleInvalidFormEntry);
+
         emailTF = new TextField();
         emailTF.relocate(750, 300);
+        emailTF.setOnKeyReleased(e->handleEmail());
 
         sceneNodes.getChildren().add(emailLabel);
+        sceneNodes.getChildren().add(invalidEmailLabel);
         sceneNodes.getChildren().add(emailTF);
     }
 
@@ -115,10 +155,17 @@ public class SignUp extends PokeSceneWithTitle {
         confirmPasswordLabel.relocate(750, 370);
         confirmPasswordLabel.setStyle("-fx-font-family: 'Arial'; -fx-font-size: 20px; -fx-font-weight: bold;");
 
+        invalidConfirmPasswordLabel = new Label("Does not match with the other password");
+        invalidConfirmPasswordLabel.relocate(910, 400);
+        invalidConfirmPasswordLabel.setVisible(false);
+        invalidConfirmPasswordLabel.setStyle(styleInvalidFormEntry);
+
         confirmPasswordTF = new PasswordField();
         confirmPasswordTF.relocate(750, 400);
-        confirmPasswordTF.setOnKeyTyped(e->handleConfirmPassword());
+        confirmPasswordTF.setOnKeyReleased(e->handleConfirmPassword());
+
         sceneNodes.getChildren().add(confirmPasswordLabel);
+        sceneNodes.getChildren().add(invalidConfirmPasswordLabel);
         sceneNodes.getChildren().add(confirmPasswordTF);
     }
 
@@ -155,36 +202,66 @@ public class SignUp extends PokeSceneWithTitle {
         sceneNodes.getChildren().add(submitButton);
     }
 
-
-    //TODO
-
     private void handleName(){
-
+        if(isPersonNoun(nameTF.getText()))
+            invalidNameLabel.setVisible(false);
+        else
+            invalidNameLabel.setVisible(true);
     }
 
     private void handleSurname(){
-
+        if(isPersonNoun(surnameTF.getText()))
+            invalidSurnameLabel.setVisible(false);
+        else
+            invalidSurnameLabel.setVisible(true);
     }
 
     private boolean isPersonNoun(String possibleNoun){
-        //TODO
-        return true;
+        Pattern pattern = Pattern.compile("^[a-zA-Z '.]*$");
+        Matcher matcher = pattern.matcher(possibleNoun);
+        return matcher.find();
+    }
+
+    private void handleEmail(){
+        if(isValidEmail(emailTF.getText()))
+            invalidEmailLabel.setVisible(false);
+        else
+            invalidEmailLabel.setVisible(true);
+    }
+
+    private boolean isValidEmail(String possibleEmail){
+        Pattern pattern = Pattern.compile("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
+        Matcher matcher = pattern.matcher(possibleEmail);
+        return matcher.find();
     }
 
     /**
      * Check if the password contains at least a capital letter and a number
      */
     private void handlePassword(){
-        //TODO
+        if(isValidPassword(passwordTF.getText()))
+            invalidPasswordLabel.setVisible(false);
+        else
+            invalidPasswordLabel.setVisible(true);
+
+    }
+
+    /**
+     * Checks if the password contains minimum eight characters, at least one letter and one number
+     */
+    private boolean isValidPassword(String possiblePassword){
+        Pattern pattern = Pattern.compile("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$");
+        Matcher matcher = pattern.matcher(possiblePassword);
+        return matcher.find();
     }
 
     private void handleConfirmPassword(){
-        //TODO
-    }
+        String password = passwordTF.getText(), confirmPassword = confirmPasswordTF.getText();
 
-    private boolean checkPasswordsMatch(){
-        //TODO
-        return true;
+        if(password.equals(confirmPassword))
+            invalidConfirmPasswordLabel.setVisible(false);
+        else
+            invalidConfirmPasswordLabel.setVisible(true);
     }
 
     private void handleSubmit(){
