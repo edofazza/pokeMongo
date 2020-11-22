dd663881222dfe64f137954384ba5613b05ef843
Olgerti Xhanej <33934387+gerti98@users.noreply.github.com>
2020-11-22 12:32:31 +0100

    aggiunta nuova classe FormValidatorPokeMongo
    Implementata inoltre la form validation anche sulla classe Settings riutilizzando gli stessi metodi

--- src/main/java/it/unipi/dii/lsmsd/pokeMongo/userInterface/Settings.java
+++ src/main/java/it/unipi/dii/lsmsd/pokeMongo/userInterface/Settings.java
@@ -1,8 +1,11 @@
 package it.unipi.dii.lsmsd.pokeMongo.userInterface;
 
+import it.unipi.dii.lsmsd.pokeMongo.utils.FormValidatorPokeMongo;
 import it.unipi.dii.lsmsd.pokeMongo.javaFXextensions.buttons.RegularButton;
 import it.unipi.dii.lsmsd.pokeMongo.javaFXextensions.labels.FieldRelatedLabel;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class Settings extends PokeSceneWithHeader {
@@ -11,10 +14,21 @@ public class Settings extends PokeSceneWithHeader {
     private TextField oldPasswordTF;
     private TextField confirmPasswordTF;
 
+    private Label invalidEmailLabel;
+    private Label invalidConfirmPasswordLabel;
+
     // RIGHT
     private TextField confirmEmailTF;
     private TextField newPasswordTF;
     private TextField countryTF;
+
+    private Label invalidConfirmEmailLabel;
+    private Label invalidPasswordLabel;
+
+    private final String styleInvalidFormEntry = "-fx-font-family: 'Arial'; -fx-font-size: 12px; " +
+            "-fx-background-color: rgb(255, 33, 26); -fx-text-fill: white; -fx-max-width: 230; -fx-padding: 3;" +
+            " -fx-border-radius: 15px; -fx-background-radius: 15px; -fx-border-width: 0.06em;" +
+            " -fx-border-color: black;";
 
     public Settings() {
         displayNewEmail();
@@ -34,17 +48,26 @@ public class Settings extends PokeSceneWithHeader {
     private void displayNewEmail() {
         FieldRelatedLabel emailLabel = new FieldRelatedLabel("New Email", 350, 170);
 
+        invalidEmailLabel = new Label("Email must follow the format:\nexample@domain.tld");
+        invalidEmailLabel.relocate(510, 190);
+        invalidEmailLabel.setVisible(false);
+        invalidEmailLabel.setStyle(styleInvalidFormEntry);
+
         newEmailTF = new TextField();
         newEmailTF.relocate(350, 200);
+        newEmailTF.setOnKeyReleased(e->FormValidatorPokeMongo.handleEmail(newEmailTF, invalidEmailLabel));
 
         sceneNodes.getChildren().add(emailLabel);
+        sceneNodes.getChildren().add(invalidEmailLabel);
         sceneNodes.getChildren().add(newEmailTF);
     }
 
     private void displayOldPassword() {
         FieldRelatedLabel oldPasswordLabel = new FieldRelatedLabel("Old Password", 350, 270);
 
-        oldPasswordTF = new TextField();
+        //TODO: query to db to find if the password matches with the old one
+
+        oldPasswordTF = new PasswordField();
         oldPasswordTF.relocate(350, 300);
 
         sceneNodes.getChildren().add(oldPasswordLabel);
@@ -54,10 +77,17 @@ public class Settings extends PokeSceneWithHeader {
     private void displayConfirmPassword() {
         FieldRelatedLabel confirmPasswordLabel = new FieldRelatedLabel("Confirm Password", 350, 370);
 
-        confirmPasswordTF = new TextField();
+        invalidConfirmPasswordLabel = new Label("Does not match with the other password");
+        invalidConfirmPasswordLabel.relocate(510, 400);
+        invalidConfirmPasswordLabel.setVisible(false);
+        invalidConfirmPasswordLabel.setStyle(styleInvalidFormEntry);
+
+        confirmPasswordTF = new PasswordField();
         confirmPasswordTF.relocate(350, 400);
+        confirmPasswordTF.setOnKeyReleased(e->FormValidatorPokeMongo.handleConfirmField(newPasswordTF, confirmPasswordTF, invalidConfirmPasswordLabel));
 
         sceneNodes.getChildren().add(confirmPasswordLabel);
+        sceneNodes.getChildren().add(invalidConfirmPasswordLabel);
         sceneNodes.getChildren().add(confirmPasswordTF);
     }
 
@@ -65,20 +95,34 @@ public class Settings extends PokeSceneWithHeader {
     private void displayConfirmEmail() {
         FieldRelatedLabel confirmEmailLabel = new FieldRelatedLabel("Confirm Email", 750, 170);
 
+        invalidConfirmEmailLabel = new Label("Does not match with the other email");
+        invalidConfirmEmailLabel.relocate(910, 200);
+        invalidConfirmEmailLabel.setVisible(false);
+        invalidConfirmEmailLabel.setStyle(styleInvalidFormEntry);
+
         confirmEmailTF = new TextField();
         confirmEmailTF.relocate(750, 200);
+        confirmEmailTF.setOnKeyReleased(e->FormValidatorPokeMongo.handleConfirmField(newEmailTF, confirmEmailTF, invalidConfirmEmailLabel));
 
         sceneNodes.getChildren().add(confirmEmailLabel);
+        sceneNodes.getChildren().add(invalidConfirmEmailLabel);
         sceneNodes.getChildren().add(confirmEmailTF);
     }
 
     private void displayNewPassword() {
         FieldRelatedLabel newPasswordLabel = new FieldRelatedLabel("New Password", 750, 270);
 
-        newPasswordTF = new TextField();
+        invalidPasswordLabel = new Label("Password must contain at least:\n8 characters, a letter and\na number");
+        invalidPasswordLabel.relocate(910, 285);
+        invalidPasswordLabel.setVisible(false);
+        invalidPasswordLabel.setStyle(styleInvalidFormEntry);
+
+        newPasswordTF = new PasswordField();
         newPasswordTF.relocate(750, 300);
+        newPasswordTF.setOnKeyReleased(e->FormValidatorPokeMongo.handlePassword(newPasswordTF, invalidPasswordLabel));
 
         sceneNodes.getChildren().add(newPasswordLabel);
+        sceneNodes.getChildren().add(invalidPasswordLabel);
         sceneNodes.getChildren().add(newPasswordTF);
     }
 
