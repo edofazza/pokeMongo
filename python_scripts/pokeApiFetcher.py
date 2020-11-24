7b2d6e7e37e3e2e87cdea97cf319942b74643c1a
Olgerti Xhanej <33934387+gerti98@users.noreply.github.com>
2020-11-24 21:54:28 +0100

    Aggiunta biology in pokemon json

--- python_scripts/pokeApiFetcher.py
+++ python_scripts/pokeApiFetcher.py
@@ -5,12 +5,18 @@ import json
 
 # 1° generation
 new_json = []
+description = ""
 
-for i in range(500, 893):
+for i in range(500, 894):
     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{i}/")
     work_string_json = response.json()
     response = requests.get(f"https://pokeapi.co/api/v2/pokemon-species/{i}/")
     work_string_json2 = response.json()
+
+    for desc in work_string_json2['flavor_text_entries']:
+        if(desc['language']['name'] == "en"):
+            description = desc['flavor_text']
+            break
 
     curr_json = {
         "id": work_string_json['id'],
@@ -18,6 +24,7 @@ for i in range(500, 893):
         "weight": work_string_json['weight'],
         "height": work_string_json['height'],
         "capture_rate": work_string_json2['capture_rate'],
+        "biology": description,
         "types": [],
         "portrait": work_string_json['sprites']['other']['official-artwork']['front_default'],
         "sprite": work_string_json['sprites']['front_default']
@@ -29,5 +36,5 @@ for i in range(500, 893):
 
     new_json.append(curr_json)
     
-with open('pokemon.json', 'w', encoding='utf-8') as f:
+with open('pokemon2.json', 'a', encoding='utf-8') as f:
     json.dump(new_json, f, ensure_ascii=False, indent=4)
