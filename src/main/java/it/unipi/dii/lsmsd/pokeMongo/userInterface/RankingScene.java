290a3a091b9b335b83a40441fc5bbc55de46ebb2
edofazza <fazzari.edoardo@gmail.com>
2020-11-24 20:27:17 +0100

    aggiunto musica al ranking

--- src/main/java/it/unipi/dii/lsmsd/pokeMongo/userInterface/RankingScene.java
+++ src/main/java/it/unipi/dii/lsmsd/pokeMongo/userInterface/RankingScene.java
@@ -2,6 +2,6 @@ package it.unipi.dii.lsmsd.pokeMongo.userInterface;
 
 public class RankingScene extends PokeSceneWithHeaderAndBackButton {
     public RankingScene() {
-
+        setSceneMusic("ranking.mp3");
     }
 }
