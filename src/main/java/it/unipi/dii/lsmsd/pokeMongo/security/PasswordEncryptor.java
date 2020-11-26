ab3670b163a9c3feead77e78f08d97b12ab29f2f
Olgerti Xhanej <33934387+gerti98@users.noreply.github.com>
2020-11-26 21:48:41 +0100

    Modificha implementazione PasswordEncryptor

--- src/main/java/it/unipi/dii/lsmsd/pokeMongo/security/PasswordEncryptor.java
+++ src/main/java/it/unipi/dii/lsmsd/pokeMongo/security/PasswordEncryptor.java
@@ -1,34 +1,16 @@
 package it.unipi.dii.lsmsd.pokeMongo.security;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-
+import org.apache.commons.codec.digest.DigestUtils;
 
 public class PasswordEncryptor {
 
     public static String encryptPassword(String plainPassword){
-        String encryptedPassword = null;
-        try {
-            byte[] salt = getSalt();
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            md.update(salt);
-            byte[] bytes = md.digest(plainPassword.getBytes());
-            StringBuilder sb = new StringBuilder();
-            for(int i=0; i< bytes.length ;i++) {
-                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
-            }
-            encryptedPassword = sb.toString();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
+        String s = "randomSalt";
+        String encryptedPassword = cipher(plainPassword, s);
         return encryptedPassword;
     }
 
-    private static byte[] getSalt() throws NoSuchAlgorithmException {
-        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
-        byte[] salt = new byte[16];
-        sr.nextBytes(salt);
-        return salt;
+    public static String cipher(String pwd, String salt) {
+        return DigestUtils.sha256Hex(pwd+salt);
     }
 }
