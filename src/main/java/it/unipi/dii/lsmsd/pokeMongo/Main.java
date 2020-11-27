18efc3551b5c35b163f99668bc49afc810828df5
Olgerti Xhanej <33934387+gerti98@users.noreply.github.com>
2020-11-27 10:45:43 +0100

    Push di prova

--- src/main/java/it/unipi/dii/lsmsd/pokeMongo/Main.java
+++ src/main/java/it/unipi/dii/lsmsd/pokeMongo/Main.java
@@ -15,6 +15,7 @@ public class Main extends Application {
      */
     @Override
     public void start(Stage primaryStage) {
+        //Commento di prova
         Scene scene = new CurrentUI().initScene();
         primaryStage.setTitle("pokeMongo");
         primaryStage.setScene(scene);
