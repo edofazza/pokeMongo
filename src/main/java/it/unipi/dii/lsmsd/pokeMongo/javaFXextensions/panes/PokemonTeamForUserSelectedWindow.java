b1d4f9cb3e7119eb35798aed3713e08691b65dcb
edofazza <fazzari.edoardo@gmail.com>
2020-11-27 20:51:05 +0100

    Window

--- src/main/java/it/unipi/dii/lsmsd/pokeMongo/javaFXextensions/panes/PokemonTeamForUserSelectedWindow.java
+++ src/main/java/it/unipi/dii/lsmsd/pokeMongo/javaFXextensions/panes/PokemonTeamForUserSelectedWindow.java
@@ -1,12 +1,40 @@
 package it.unipi.dii.lsmsd.pokeMongo.javaFXextensions.panes;
 
+import it.unipi.dii.lsmsd.pokeMongo.javaFXextensions.buttons.FilterPokemonResultButton;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
 public class PokemonTeamForUserSelectedWindow extends Pane {
+
+    // TODO: GET THE POKEMON TO SHOW AND THE VALUE RELATED TO IT
     public PokemonTeamForUserSelectedWindow(int x, int y) {
         relocate(x, y);
         setPrefSize(260, 90);
 
-        setStyle("-fx-background-color: #ded8d8;");
+        setStyle("-fx-border-color: #ded8d8; " +
+                "-fx-background-color: transparent;" +
+                "-fx-border-radius: 10;");
+
+        displayPokemonSprite();
+        displayPokemonName();
+        displayPokemonPoints();
+    }
+
+    private void displayPokemonSprite() {
+        ImageView sprite = new ImageView("file:img/sprites/7.png");
+        sprite.setFitHeight(90);
+        sprite.setFitWidth(90);
+
+        getChildren().add(sprite);
+    }
+
+    private void displayPokemonName() {
+        FilterPokemonResultButton pokemon = new FilterPokemonResultButton("Squirtle", 95, 28);
+
+        getChildren().add(pokemon);
+    }
+
+    private void displayPokemonPoints() {
+
     }
 }
