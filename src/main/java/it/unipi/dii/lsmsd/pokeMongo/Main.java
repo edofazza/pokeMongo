74b11be3e3028c03d7ed9e5ba09e3bbecd477169
edofazza <fazzari.edoardo@gmail.com>
2020-11-28 18:23:32 +0100

    Removed a test comment

--- src/main/java/it/unipi/dii/lsmsd/pokeMongo/Main.java
+++ src/main/java/it/unipi/dii/lsmsd/pokeMongo/Main.java
@@ -15,7 +15,6 @@ public class Main extends Application {
      */
     @Override
     public void start(Stage primaryStage) {
-        //Commento di prova
         Scene scene = new CurrentUI().initScene();
         primaryStage.setTitle("pokeMongo");
         primaryStage.setScene(scene);
