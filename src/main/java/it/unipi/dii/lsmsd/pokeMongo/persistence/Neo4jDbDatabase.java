0a42ba50f66dee404e7834930723212f1ad6f504
Mirco-Ramo <m.ramo@studenti.unipi.it>
2020-12-05 14:17:05 +0100

    crud ops in Neo4jDbDatabase.java

--- src/main/java/it/unipi/dii/lsmsd/pokeMongo/persistence/Neo4jDbDatabase.java
+++ src/main/java/it/unipi/dii/lsmsd/pokeMongo/persistence/Neo4jDbDatabase.java
@@ -32,21 +32,106 @@ public abstract class Neo4jDbDatabase implements Database {
         return driver;
     }
 
-    @Override
-    public abstract boolean insert(ArrayList<Object> toInsert);
+    public boolean insert(ArrayList<Object> toInsert){
+        getConnection();
+        for(Object query: toInsert){
+            if(!(query instanceof String)){
+                closeConnection();
+                return false;
+            }
+            try (Session session = driver.session()) {
+                session.writeTransaction((TransactionWork<Void>) tx -> {
+                    tx.run((String)query);
+                    return null;
+                });
+            }
+        }
+        closeConnection();
+        return true;
+    }
 
     @Override
-    public abstract boolean insert(Object toInsert);
+    public boolean insert(Object query){
+        if(!(query instanceof String)){
+            return false;
+        }
+        startConnection();
+        try (Session session = driver.session()) {
+            session.writeTransaction((TransactionWork<Void>) tx -> {
+                tx.run((String)query);
+                return null;
+            });
+        }
+        closeConnection();
+        return true;
+    }
 
     @Override
-    public abstract boolean remove(Object o);
+    public boolean remove(Object query){
+        if(!(query instanceof String))
+            return false;
+        startConnection();
+        try (Session session = driver.session()) {
+            session.writeTransaction((TransactionWork<Void>) tx -> {
+                tx.run((String)query);
+                return null;
+            });
+        }
+        closeConnection();
+        return true;
+    }
 
     @Override
-    public abstract ArrayList<Object> getAll();
+    public ArrayList<Object> getAll(){
+        ArrayList<Object> records = new ArrayList<>();
+        startConnection();
+        try (Session session = driver.session()) {
+            session.readTransaction((TransactionWork<Void>) tx -> {
+                Result result = tx.run("MATCH (n) RETURN n");
+                while (result.hasNext()) {
+                    Record r = result.next();
+                    records.add(r);
+                }
+                return null;
+            });
+        }
+        closeConnection();
+        return records;
+    }
 
     @Override
-    public abstract ArrayList<Object> getWithFilter(Object filter);
+    public ArrayList<Object> getWithFilter(Object query){
+        if(!(query instanceof String))
+            return null;
+        ArrayList<Object> records = new ArrayList<>();
+        startConnection();
+        try (Session session = driver.session()) {
+            session.readTransaction((TransactionWork<Void>) tx -> {
+                Result result = tx.run((String)query);
+                while (result.hasNext()) {
+                    Record r = result.next();
+                    records.add(r);
+                }
+                return null;
+            });
+        }
+        closeConnection();
+        return records;
+    }
 
     @Override
-    public abstract boolean update(Object target, Object newValue);
+    public boolean update(Object target, Object newValue){
+        if(!(target instanceof String) || !(newValue instanceof String))
+            return false;
+        startConnection();
+        try (Session session = driver.session()) {
+            session.writeTransaction((TransactionWork<Void>) tx -> {
+                tx.run(target + " " + newValue);
+                return null;
+            });
+        }
+        closeConnection();
+        return true;
+    }
+
 }
