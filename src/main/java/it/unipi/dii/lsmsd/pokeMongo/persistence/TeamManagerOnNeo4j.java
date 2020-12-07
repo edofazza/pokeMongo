c4f3140a7e74bd6288fb3ba8efe3f2229eb44e3c
Mirco-Ramo <m.ramo@studenti.unipi.it>
2020-12-07 10:33:05 +0100

    filled and documented TeamManager.java Inserted Override annotations on TeamManagerOnNeo4j.java

--- src/main/java/it/unipi/dii/lsmsd/pokeMongo/persistence/TeamManagerOnNeo4j.java
+++ src/main/java/it/unipi/dii/lsmsd/pokeMongo/persistence/TeamManagerOnNeo4j.java
@@ -17,7 +17,7 @@ import static org.neo4j.driver.Values.parameters;
 
 public class TeamManagerOnNeo4j extends Neo4jDbDatabase implements TeamManager{
 
-
+    @Override
     @VisibleForTesting
     // eventualmente insertAPokemonIntoTeam(Team t, int slot)
     public boolean insertAPokemonIntoTeam(User u, Pokemon p, int slot) throws SlotAlreadyOccupiedException{
@@ -36,17 +36,20 @@ public class TeamManagerOnNeo4j extends Neo4jDbDatabase implements TeamManager{
         return (d.get("link_count").asInt() > 0);
     }
 
+    @Override
     public boolean deletePokemonFromTeamBySlot(User u, int slot){
         String query = "MATCH (n:User)-[w:HAS]->(p:Pokemon) WHERE n.username = $username and w.slot = $slot DELETE w";
         return remove(query, parameters("username", u.getUsername(), "slot", slot));
     }
 
+    @Override
     //TODO: elimina anche le relazioni di match
     public boolean deletePokemon(String pokemonName){
         String query = "MATCH (p:Pokemon) WHERE p.name = $name DETACH DELETE p";
         return remove(query, parameters("name", pokemonName));
     }
 
+    @Override
     public boolean addPokemon(Pokemon p) throws DuplicatePokemonException{
         if(pokemonAlreadyExist(p))
             throw new DuplicatePokemonException();
@@ -66,6 +69,7 @@ public class TeamManagerOnNeo4j extends Neo4jDbDatabase implements TeamManager{
     }
 
 
+    @Override
     @VisibleForTesting
     //eventualmente ritorna un Team
     public Pokemon[] getUserTeam(User target) {
@@ -85,6 +89,7 @@ public class TeamManagerOnNeo4j extends Neo4jDbDatabase implements TeamManager{
         return team;
     }
 
+
     public ArrayList<Pokemon> getBestPokemon() {
         ArrayList<Pokemon> pokemonArrayList = new ArrayList<>();
         String query = "MATCH ()-[h:HAS]->(p:Pokemon) return p.name, count(h) AS held, p.sprite ORDER BY held DESC LIMIT " +  + ConfigDataHandler.getInstance().configData.numRowsRanking;
@@ -99,6 +104,7 @@ public class TeamManagerOnNeo4j extends Neo4jDbDatabase implements TeamManager{
         return getPokemons(pokemonArrayList, res);
     }
 
+    @Override
     public ArrayList<Pokemon> getFriendsTeam(User u){
         //Prima ottengo user amici
         //Da mongo db ottengo il punteggio degli amici
