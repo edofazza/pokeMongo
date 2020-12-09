7ea90f2e3c5a0f500e94edf977a7be461e82b0ea
Mirco-Ramo <m.ramo@studenti.unipi.it>
2020-12-09 11:48:37 +0100

    created server module with preliminary structure

---
+++ PokeMongoAnalyticServer/src/main/java/persistence/Database.java
@@ -0,0 +1,63 @@
+package persistence;
+
+import java.util.ArrayList;
+
+
+/**
+ * Shared interface among Databases, defines remote connections and structures of basic CRUD operations
+ */
+public interface Database {
+
+    /**
+     * starts the connection with the remote Database
+     */
+    void startConnection();
+
+    /**
+     * closes the connection with the remote Database
+     */
+    void closeConnection();
+
+
+    /**
+     * insert 1 ore more objects into the database
+     * @param toInsert ArrayList of arbitrary objects to insert into the Database
+     * @return true if at least one element has been added
+     */
+    boolean insert(ArrayList<Object> toInsert);
+
+    /**
+     * insert 1 object into the database
+     * @param toInsert ArrayList of arbitrary objects to insert into the Database
+     * @return true if exactly one element has been added
+     */
+    boolean insert(Object toInsert);
+
+    /**
+     * remove 1 ore more objects from the database
+     * @param o is an Object to remove or a query to submit
+     * @return true if at least one element has been removed
+     */
+    boolean remove(Object o);
+
+    /**
+     * gets all the elements
+     * @return a list of retrieved elements
+     */
+    ArrayList<Object> getAll();
+
+    /**
+     *
+     * @param filter query or filter to submit to the db
+     * @return a list of retrieved elements
+     */
+    ArrayList<Object> getWithFilter(Object filter);
+
+    /**
+     *
+     * @param target query or object to update
+     * @param newValue new value for the target
+     * @return true if at least one object is updated
+     */
+    boolean update(Object target, Object newValue);
+}
---
+++ PokeMongoAnalyticServer/src/main/java/persistence/MongoDbDatabase.java
@@ -0,0 +1,88 @@
+package persistence;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+//import it.unipi.dii.lsmsd.pokeMongo.utils.Logger;
+import org.bson.Document;
+
+import java.util.ArrayList;
+
+public abstract class MongoDbDatabase implements Database{
+    private MongoClient connection;
+
+    private String host="localhost";
+    private int port = 27017;
+    private String dbName="pokeMongo";
+
+    @Override
+    public void startConnection(){
+        connection=MongoClients.create("mongodb://" + host + ":" + port);
+    }
+
+    @Override
+    public void closeConnection() {
+        //Logger.vlog("Closing connection with MongoDB");
+        if(connection!=null) {
+            connection.close();
+            connection = null;
+        }
+    }
+
+    private MongoClient getConnection() {
+        if(connection==null)
+            startConnection();
+        return connection;
+    }
+
+    private MongoDatabase getDatabase(){
+        //Logger.vlog("Getting " + dbName + " Database");
+        return getConnection().getDatabase(dbName);
+    }
+
+    public MongoCollection<Document> getCollection(String name){
+        //Logger.vlog("Getting " + name + " collection");
+        return getDatabase().getCollection(name);
+    }
+
+    public void dropCollection(String name){
+        //Logger.vlog("Dropping " + name + " Database");
+        getCollection(name).drop();
+    }
+
+    @Override
+    public boolean insert(ArrayList<Object> toInsert){
+        showError();
+        return false;
+    }
+
+    @Override
+    public boolean insert(Object toInsert){
+        showError();
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o){
+        showError();
+        return false;
+    }
+
+    @Override
+    public abstract ArrayList<Object> getAll();
+
+    @Override
+    public abstract ArrayList<Object> getWithFilter(Object filter);
+
+    @Override
+    public boolean update(Object target, Object newValue){
+        showError();
+        return false;
+    }
+
+    private void showError(){
+        Thread.currentThread().interrupt();
+        System.out.println("WRITE ACCESS NOT ALLOWED FOR THREAD " + Thread.currentThread().getName());
+    }
+}
