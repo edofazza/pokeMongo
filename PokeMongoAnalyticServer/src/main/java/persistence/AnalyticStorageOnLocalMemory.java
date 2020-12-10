ae31a246430ae3890e7d10e65e790bd8787e52d7
Mirco-Ramo <m.ramo@studenti.unipi.it>
2020-12-10 19:11:26 +0100

    Completed AnalyticStorageOnLocalMemory.java

--- PokeMongoAnalyticServer/src/main/java/persistence/AnalyticStorageOnLocalMemory.java
+++ PokeMongoAnalyticServer/src/main/java/persistence/AnalyticStorageOnLocalMemory.java
@@ -5,12 +5,13 @@ import java.util.*;
 class AnalyticStorageOnLocalMemory implements AnalyticStorage{
     private final int numDays;
     private static AnalyticStorageOnLocalMemory instance;
-    private static int index=0;
-    private static int howManyValid=0;
+    private static int indexLastLogin=0;
+    private static int indexUserCounter=0;
+    private static int indexUserCounterByCountry=0;
 
-    private static int[] circularLastLoginsCounter;
+    private static long[] circularLastLoginsCounter;
 
-    private static int[] circularUserCounter;
+    private static long[] circularUserCounter;
 
     private static List<Map<String, Long>> circularUserCounterByCountry;
 
@@ -26,8 +27,8 @@ class AnalyticStorageOnLocalMemory implements AnalyticStorage{
 
     AnalyticStorageOnLocalMemory(int days){
         this.numDays=days;
-        circularLastLoginsCounter = new int[days];
-        circularUserCounter = new int[days];
+        circularLastLoginsCounter = new long[days];
+        circularUserCounter = new long[days];
         circularUserCounterByCountry = new ArrayList<>();
     }
 
@@ -35,33 +36,63 @@ class AnalyticStorageOnLocalMemory implements AnalyticStorage{
         this(30);
     }
 
+
+    private static void setIndex(int index){
+        index++;
+    }
+
+    private long[] getFromCircularArray(int index, long[] array){
+        long[] toReturn = new long[numDays];
+        for(int i=0, cursor=(index+1)%numDays; i<numDays && cursor!=index;cursor=(cursor+1)%numDays){
+            if(array[cursor]!=0) {
+                toReturn[i] = array[cursor];
+                i++;
+            }
+        }
+        return toReturn;
+    }
+
+
     @Override
     public long[] getLastLogins() {
-        return new long[0];
+        return getFromCircularArray(indexLastLogin ,circularLastLoginsCounter);
     }
 
     @Override
     public long[] getUserNumber() {
-        return new long[0];
+        return getFromCircularArray(indexUserCounter, circularUserCounter);
     }
 
     @Override
     public List<Map<String, Long>> getUserNumberByCountry() {
-        return new ArrayList<>();
+        List<Map<String, Long>> toReturn = new ArrayList<>();
+        for(int i=0, cursor=(indexUserCounterByCountry+1)%numDays;
+                i<numDays && cursor!=indexUserCounterByCountry;
+                cursor=(cursor+1)%numDays)
+        {
+            if(circularUserCounterByCountry.get(cursor)!=null) {
+                toReturn.add(i, circularUserCounterByCountry.get(cursor));
+                i++;
+            }
+        }
+        return toReturn;
     }
 
     @Override
     public void setLastLogin(long counted) {
-
+        setIndex(indexLastLogin);
+        circularLastLoginsCounter[indexLastLogin]=counted;
     }
 
     @Override
     public void setUserNumber(long counted) {
-
+        setIndex(indexUserCounter);
+        circularUserCounter[indexUserCounter]=counted;
     }
 
     @Override
     public void setUserNumberByCountry(Map<String, Long> counted) {
-
+        setIndex(indexUserCounterByCountry);
+        circularUserCounterByCountry.add(indexUserCounterByCountry, counted);
     }
 }
