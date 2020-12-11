702f16ff590d08b6b6718edb127d2ec0d0b74322
edofazza <fazzari.edoardo@gmail.com>
2020-12-11 18:03:58 +0100

    Update CipherFriendsGenerator.java

--- src/main/java/it/unipi/dii/lsmsd/pokeMongo/CipherCodeGenerator/CipherFriendsGenerator.java
+++ src/main/java/it/unipi/dii/lsmsd/pokeMongo/CipherCodeGenerator/CipherFriendsGenerator.java
@@ -1,4 +1,41 @@
 package it.unipi.dii.lsmsd.pokeMongo.CipherCodeGenerator;
 
+import it.unipi.dii.lsmsd.pokeMongo.bean.User;
+import it.unipi.dii.lsmsd.pokeMongo.persistence.UserManager;
+import it.unipi.dii.lsmsd.pokeMongo.persistence.UserManagerFactory;
+
+import java.util.ArrayList;
+
 public class CipherFriendsGenerator {
+    public static void main(String[] args) {
+        String s = new String();
+
+        // TAKE ALL THE USERS
+        UserManager userManager = UserManagerFactory.buildManager();
+        ArrayList<User> allUsers = userManager.getEveryUser();
+
+        for (User u: allUsers) {
+            if (u.isAdmin())
+                continue;
+
+            int index;
+            if (allUsers.size() > 15)
+                index = (int) Math.round(Math.random() * 15);
+            else
+                index = (int) Math.round(Math.random() * allUsers.size());
+
+            for (int i = 0; i < index; i++) {
+                int indexUser = (int) Math.round(Math.random() * allUsers.size());
+
+                String to = allUsers.get(indexUser).getUsername();
+                if(!u.getUsername().equals(allUsers.get(indexUser).getUsername()))
+                    s += "MATCH (from:User) WHERE from.username = \"" + u.getUsername() + "\" MATCH (to:User) " +
+                            " WHERE to.username = " + to + " MERGE (from)-[:FOLLOW]->(to)\n";
+            }
+
+        }
+
+        System.out.println(s);
+    }
+
 }
