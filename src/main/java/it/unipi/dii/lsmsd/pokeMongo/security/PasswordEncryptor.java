2ec8d763417951ae7a2379a39bde875b512738f3
edofazza <fazzari.edoardo@gmail.com>
2020-12-19 18:20:10 +0100

    Update PasswordEncryptor.java

--- src/main/java/it/unipi/dii/lsmsd/pokeMongo/security/PasswordEncryptor.java
+++ src/main/java/it/unipi/dii/lsmsd/pokeMongo/security/PasswordEncryptor.java
@@ -6,6 +6,11 @@ import org.apache.commons.codec.digest.DigestUtils;
 
 public class PasswordEncryptor {
 
+    /**
+     *
+     * @param plainPassword the password inserted by the user (without encryption)
+     * @return a string containing the encrypted password
+     */
     @VisibleForTesting
     public static String encryptPassword(String plainPassword){
         String s = "randomSalt";
@@ -14,6 +19,12 @@ public class PasswordEncryptor {
         return encryptedPassword;
     }
 
+    /**
+     *
+     * @param pwd password inserted by the user
+     * @param salt a salt we added for the encryption
+     * @return a string that is the digested password
+     */
     public static String cipher(String pwd, String salt) {
         return DigestUtils.sha256Hex(pwd+salt);
     }
