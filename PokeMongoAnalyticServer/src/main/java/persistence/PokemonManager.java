13dc6874acf6a5a51fd5c7d0b347442d978b10bd
Mirco-Ramo <m.ramo@studenti.unipi.it>
2020-12-21 11:05:39 +0100

    javadoc on Server side

--- PokeMongoAnalyticServer/src/main/java/persistence/PokemonManager.java
+++ PokeMongoAnalyticServer/src/main/java/persistence/PokemonManager.java
@@ -9,12 +9,25 @@ import java.util.*;
 public interface PokemonManager {
 
     /**
-     * <h2>>Searches all the Pokemon inside the DB</h2>
+     * <h2>Searches all the Pokemon inside the DB</h2>
      * @return an Arraylist containing all the Pokemon that satisfy search conditions
      */
     ArrayList<Pokemon> getEveryPokemon();
 
+    /**
+     * <h2>Updates one or more fields of one or more Pokemon</h2>
+     * @param target query to target Pokemon to update
+     * @param newValue query to express new value of the field(s)
+     * @return true if the update is correctly submitted
+     */
     boolean update(Object target, Object newValue);
+
+    /**
+     * <h2>Updates old_p fields according to new_p</h2>
+     * @param old_p Pokemon to modify
+     * @param new_p Pokemon with updated fields
+     * @return number of modified Pokemon
+     */
     long updatePokemon(Pokemon old_p, Pokemon new_p);
 
 }
--- PokeMongoAnalyticServer/src/main/java/persistence/TeamManager.java
+++ PokeMongoAnalyticServer/src/main/java/persistence/TeamManager.java
@@ -9,7 +9,22 @@ import java.util.*;
 
 public interface TeamManager {
 
+    /**
+     * Counts how many users own a specific Pokemon
+     * @param p Pokemon on wich owners are counted
+     * @return number of users that own p
+     */
     int getUsersNumberThatOwnAPokemon(Pokemon p);
+
+    /**
+     * Maps each Pokemon with the number of users that own ut
+     * @return a List of pairs with this mapping
+     */
     List<Pair<String, Integer>> getUsersNumberThatOwnsAPokemonNotFiltered();
+
+    /**
+     * updates catch rates of each Pokemon
+     * @param catch_rates maps each Pokemon with the new value of the catch rate
+     */
     void updateCatchRateOfPokemon(List<PokemonAndCatchRate> catch_rates);
 }
