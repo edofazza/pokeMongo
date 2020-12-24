9115020011db0d8f62c4fa4d2149ca2b0a96955e
edofazza <fazzari.edoardo@gmail.com>
2020-12-24 10:50:29 +0100

    Logger test for null text

--- src/main/java/it/unipi/dii/lsmsd/pokeMongo/utils/Logger.java
+++ src/main/java/it/unipi/dii/lsmsd/pokeMongo/utils/Logger.java
@@ -1,14 +1,15 @@
 package it.unipi.dii.lsmsd.pokeMongo.utils;
 
+import com.google.common.annotations.VisibleForTesting;
 import it.unipi.dii.lsmsd.pokeMongo.config.ConfigDataHandler;
-
-import java.io.*;
-import java.nio.file.*;
 
 public class Logger {
 
-
+    @VisibleForTesting
     public static void warning(String text){
+        if (nullMsg(text))
+            return;
+
         if(ConfigDataHandler.getInstance().configData.verbosityLevel >= 1){
             LoggerThread lt = new LoggerThread("[WARNING] " + text);
             lt.start();
@@ -17,6 +18,9 @@ public class Logger {
     }
 
     public static void error(String text){
+        if (nullMsg(text))
+            return;
+
         if(ConfigDataHandler.getInstance().configData.verbosityLevel >= 1){
             LoggerThread lt = new LoggerThread("[ERROR] " + text);
             lt.start();
@@ -24,6 +28,9 @@ public class Logger {
     }
 
     public static void log(String text){
+        if (nullMsg(text))
+            return;
+
         if(ConfigDataHandler.getInstance().configData.verbosityLevel >= 1){
             LoggerThread lt = new LoggerThread("[LOG] " + text);
             lt.start();
@@ -31,6 +38,9 @@ public class Logger {
     }
 
     public static void vlog(String text){
+        if (nullMsg(text))
+            return;
+
         if(ConfigDataHandler.getInstance().configData.verbosityLevel >= 2){
             LoggerThread lt = new LoggerThread("[VLOG] " + text);
             lt.start();
@@ -38,12 +48,21 @@ public class Logger {
     }
 
     public static void vvlog(String text){
+        if (nullMsg(text))
+            return;
+
         if(ConfigDataHandler.getInstance().configData.verbosityLevel >= 3){
             LoggerThread lt = new LoggerThread("[VVLOG] " + text);
             lt.start();
         }
     }
 
-
+    private static boolean nullMsg(String text) {
+        if (text == null) {
+            warning("NullPointerException avoided");
+            return true;
+        }
+        return false;
+    }
 
 }
---
+++ src/test/java/it/unipi/dii/lsmsd/pokeMongo/utils/LoggerTest.java
@@ -0,0 +1,14 @@
+package it.unipi.dii.lsmsd.pokeMongo.utils;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class LoggerTest {
+
+    @Test
+    public void WHEN_warning_invoked_with_null_params_THEN_NullPointerException_thrown() {
+        Assertions.assertThrows(NullPointerException.class, ()->{
+            Logger.warning(null);
+        });
+    }
+}
