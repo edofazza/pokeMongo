619bbbdaaac25adaa1d33964fae1396437ca8f37
Olgerti Xhanej <33934387+gerti98@users.noreply.github.com>
2020-12-27 21:05:14 +0100

    reupdated dump

--- src/main/java/it/unipi/dii/lsmsd/pokeMongo/utils/autoLog/AutoLogIn.java
+++ src/main/java/it/unipi/dii/lsmsd/pokeMongo/utils/autoLog/AutoLogIn.java
@@ -16,7 +16,7 @@ public class AutoLogIn {
         ArrayList<User> allUsers = userManager.getEveryUser();
 
         for (User user: allUsers) {
-            if (Math.random() < .7)
+            if (Math.random() < .997)
                 continue;
             userManager.login(user.getUsername(),user.getName() + user.getSurname() + "000");
 
